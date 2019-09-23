d751e737218ca7e5fa7ad7f8391d6726e55d0500
Gon Shimoni <gon.shimoni@gmail.com>
2019-09-23 10:33:18 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by roni newman on 23/09/2019.
+//  Copyright © 2019 Gon Shimoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
